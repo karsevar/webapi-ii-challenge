bb9c9b7d3c1bf989df00519036f81f6ed7e55dea
masonkarsevar <masonkarsevar@gmail.com>
2019-09-03 12:11:44 -0700

    posts are received by get api call

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const server = express();
 
 server.use(express.json());
 
+// Server test on the root url without /api extension.
 server.get('/', (req, res) => {
     res.status(200).json({message: 'Success!!!'})
+})
+
+// GET request for all the posts in database:
+server.get('/api/posts', (req, res) => {
+    db.find() 
+        .then(result => res.status(201).json(result))
+        .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }))
 })
 
 module.exports = server
